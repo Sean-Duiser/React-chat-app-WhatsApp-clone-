60b26c60cca9ebeee6b2222f076952e725723d6d
Sean Duiser <duzer84@gmail.com>
2022-07-06 10:32:38 -0400

    hooked up firebase

---
+++ whazzapp/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAu0zDWVwjB9V6GrnjjnYj8DCIgyXJQt2k",
+  authDomain: "whazzapp-9aaa5.firebaseapp.com",
+  projectId: "whazzapp-9aaa5",
+  storageBucket: "whazzapp-9aaa5.appspot.com",
+  messagingSenderId: "282560351609",
+  appId: "1:282560351609:web:1a0d2a9217d3adee36f3a8",
+  measurementId: "G-8J5EFX76CM"
+};
