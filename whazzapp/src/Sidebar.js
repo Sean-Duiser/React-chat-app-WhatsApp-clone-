d6aa3b46dce2e946f7cd1936e3d7555d470f34f3
Sean Duiser <duzer84@gmail.com>
2022-07-07 10:31:06 -0400

    icons showing in sidebar

--- whazzapp/src/Sidebar.js
+++ whazzapp/src/Sidebar.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import { Avatar } from '@mui/material';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import './Sidebar.css'
 
 function Sidebar() {
   return (
     <div className='sidebar'>
       <div className='sidebar__header'>
+        <Avatar />
 
       </div>
 
       <div className='sidebar__search'>
+        <DonutLargeIcon />
+        <ChatIcon />
+        <MoreVertIcon />
 
       </div>
 
