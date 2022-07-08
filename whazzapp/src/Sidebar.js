a215e26931a7db7688d871026bd1fcc12232717a
Sean Duiser <duzer84@gmail.com>
2022-07-08 10:11:42 -0400

    search container styling

--- whazzapp/src/Sidebar.js
+++ whazzapp/src/Sidebar.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Avatar } from '@mui/material';
+import { Avatar, IconButton } from '@mui/material';
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchOutlined from '@mui/icons-material/SearchOutlined';
 import './Sidebar.css'
 
 function Sidebar() {
@@ -10,13 +11,24 @@ function Sidebar() {
     <div className='sidebar'>
       <div className='sidebar__header'>
         <Avatar />
-
+        <div className='sidebar__headerRight'>
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
       </div>
 
       <div className='sidebar__search'>
-        <DonutLargeIcon />
-        <ChatIcon />
-        <MoreVertIcon />
+        <div className='sidebar__searchContainer'>
+          <SearchOutlined />
+          <input placeholder='Search or start new chat' type='text' />
+        </div>
 
       </div>
 
